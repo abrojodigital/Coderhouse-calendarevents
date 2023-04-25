1b6d6838a5faa573f971d16ebbe84136891eb955
abrojodigital <abrojodigital@gmail.com>
2023-04-25 17:02:18 -0300

    Se quita flex a Modal y se ajusta a pantalla

--- src/components/modalokcancel/styles.js
+++ src/components/modalokcancel/styles.js
@@ -3,10 +3,15 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
 
   modalContainer: {
-    flex: 1,
-    backgroundColor: '#ddd',
+    width: '80%',
+    height: '50%',
+    backgroundColor: 'rgba(221,221,221,0.7)',
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'center',
+    position: 'absolute',
+    top: '25%',
+    left: '10%',
   },
   modalTitle: {
     fontSize: 16,
